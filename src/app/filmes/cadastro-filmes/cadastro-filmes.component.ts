f7d0dec3786751e380eba98dbd8232d2e506688b
Breno Rev <silvabreno462@gmail.com>
2021-08-23 00:17:23 -0300

    Criando um modal para mostrar mensagem de sucesso

--- src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
+++ src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
 import { FilmesService } from 'src/app/core/filmes.service';
+import { AlertaComponent } from 'src/app/shared/components/alerta/alerta.component';
 import { ValidarCamposService } from 'src/app/shared/components/campos/validar-campos.service';
 import { Filme } from 'src/app/shared/models/filme';
 
@@ -16,9 +18,10 @@ export class CadastroFilmesComponent implements OnInit {
   generos: Array<string>;
 
   constructor(  public validacao: ValidarCamposService,
+                public dialog: MatDialog,
                 private fb: FormBuilder,
                 private filmeService: FilmesService
-              ) { }
+                ) { }
 
   get f(){
     return this.cadastro.controls;
@@ -64,7 +67,7 @@ export class CadastroFilmesComponent implements OnInit {
 
   private salvar(filme: Filme): void {
     this.filmeService.salvar(filme).subscribe(() => {
-      alert("Salvo com sucesso!");
+      const dialogRef = this.dialog.open(AlertaComponent);
     },
     () => {
       alert("Erro ao salvar...");
